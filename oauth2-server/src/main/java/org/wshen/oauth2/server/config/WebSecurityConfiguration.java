b08e50ba2314374593ddf531be2d1a90b9b53f89
vue01 <wyj8574@163.com>
2020-10-25 18:41:22 +0800

    内存模式测试

--- oauth2-server/src/main/java/org/wshen/oauth2/server/config/WebSecurityConfiguration.java
+++ oauth2-server/src/main/java/org/wshen/oauth2/server/config/WebSecurityConfiguration.java
@@ -1,12 +1,10 @@
 package org.wshen.oauth2.server.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @ClassName WebSecurityConfiguration
@@ -21,5 +19,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("123456")
+                .roles("ADMIN")
+                .and()
+                .withUser("user").password("123456")
+                .roles("USER");
+    }
 }
